fd69a08d8cd7fb73a986acb3d794380ab720fc75
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-04-30 10:07:44 +0200

    modifiche

--- censura.php
+++ censura.php
@@ -1,4 +1,8 @@
 <?php
+$paragrafo = $_POST['testo'];
+$censura = $_POST['censura'];
+
+$testoCensurato = str_replace($censura, '***', $paragrafo);
 
 ?>
 
@@ -10,6 +14,11 @@
     <title>Document</title>
 </head>
 <body>
-    
+   <h2>Paragrafo</h2>
+   <p><?php echo $paragrafo?></p> 
+   <p>lunghezza: <?php echo strlen($paragrafo)?></p>
+   <h2>Paragrafo censurato</h2>
+   <p><?php echo $testoCensurato?></p>
+   <p>lunghezza: <?php echo strlen($testoCensurato)?></p>
 </body>
 </html>
--- index.php
+++ index.php
@@ -9,6 +9,11 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>inserisci testo</h1>
+    <form action="censura.php" method="post">
+        <input type="text" name="testo">
+        <input type="text" name="censura">
+        <input type="submit">
+    </form>
 </body>
 </html>
